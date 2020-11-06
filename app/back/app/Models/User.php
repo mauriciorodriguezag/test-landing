6468466086f79a690a75c9c5f7ec0d17afbfb095
Mauricio <mauriciorodriugezag@gmail.com>
2020-11-06 16:16:28 -0500

    First commit

---
+++ app/back/app/Models/User.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
+
+class User extends Model
+{
+    use HasFactory, Notifiable;
+    
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name',
+        'lastname',
+        'document_id',
+        'city',
+        'phone',
+        'mail',
+        'authorization'
+    ];
+}
